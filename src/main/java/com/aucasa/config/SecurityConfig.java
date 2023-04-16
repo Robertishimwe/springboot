400ae04f25df2d357606874c6190eff632f3013f
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-16 12:17:29 +0000

    test

--- src/main/java/com/aucasa/config/SecurityConfig.java
+++ src/main/java/com/aucasa/config/SecurityConfig.java
@@ -83,12 +83,11 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
       configuration.setAllowedOrigins(Arrays.asList("*"));
-      configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-      configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-
+      configuration.setAllowedMethods(Arrays.asList("*"));
+      configuration.setAllowedHeaders(Arrays.asList("*"));
+      configuration.setAllowCredentials(true);
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       source.registerCorsConfiguration("/**", configuration);
-
       return source;
   }
 }
